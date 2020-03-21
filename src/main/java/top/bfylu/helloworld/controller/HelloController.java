4d9c39e87cbae4f574b3ef6a60eec17a2ee0bd64
Lu Jian <18279544224@163.com>
2020-03-21 20:23:55 +0800

    spring cloud demo helloworld

---
+++ src/main/java/top/bfylu/helloworld/controller/HelloController.java
@@ -0,0 +1,13 @@
+package top.bfylu.helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("hello")
+    public String index() {
+        return "Greetings from Spring Boot";
+    }
+}
---
+++ src/main/java/top/bfylu/helloworld/controller/LucyController.java
@@ -0,0 +1,28 @@
+package top.bfylu.helloworld.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.bfylu.helloworld.config.mutildb.ConfigBean;
+import top.bfylu.helloworld.model.User;
+
+@RestController
+@EnableConfigurationProperties({ConfigBean.class, User.class})
+public class LucyController {
+    @Autowired
+    ConfigBean configBean;
+
+    @RequestMapping(value = "/lucy")
+    public String miya() {
+        return configBean.getGreeting() + "-" + configBean.getName() + "-" + configBean.getUuid() + "-" + configBean.getMax();
+    }
+
+    @Autowired
+    User user;
+
+    @RequestMapping(value = "user")
+    public String user() {
+        return user.getName() + ":" + user.getAge();
+    }
+}
