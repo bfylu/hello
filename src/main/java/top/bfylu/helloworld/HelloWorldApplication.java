48324a1bf6fa0c3fcbb8a4b02c4f7e9f44f7d5e3
Lu Jian <18279544224@163.com>
2020-03-22 22:54:25 +0800

    pom.xml 移除tomcat

--- src/main/java/top/bfylu/helloworld/HelloWorldApplication.java
+++ src/main/java/top/bfylu/helloworld/HelloWorldApplication.java
@@ -2,9 +2,16 @@ package top.bfylu.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HelloWorldApplication {
+public class HelloWorldApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder){
+        return applicationBuilder.sources(HelloWorldApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
